4dc796f12757784c94749c9254ef22a13930d580
Navneet Kumar <navneetharsh3@gmail.com>
2022-03-14 10:23:40 +0530

    third

--- src/Components/AddHouse/AddHouse.jsx
+++ src/Components/AddHouse/AddHouse.jsx
@@ -1,36 +1,51 @@
+import { useState } from "react";
 
 
 
 export const AddHouse = () => {
-  
+     const [form, setForm] = useState({
+       name:"",
+       ownerName:"",
+       address:"",
+       areaCode:"",
+       rent:"",
+       preferredTenant:"",
+       image:"",
+     })
+
+     const formHandle=(e)=>{
+       const {id, value} = e.target;
+       setForm({...form, [id]:value});
+     }
+    
   return (
     <div className="addHouseContainer">
       <form>
         <label>name</label>
-        <input type="text" className="name" value={""} required />
+        <input id="name" type="text" className="name" value={""} onChange={formHandle}  required />
         <br />
         <label>ownerName</label>
-        <input value={""} type="text" className="ownerName" required />
+        <input id="ownerName" value={""} onChange={formHandle} type="text" className="ownerName" required />
         <br />
         <label>address</label>
-        <input value={""} type="text" className="address" required />
+        <input id="address" value={""} onChange={formHandle} type="text" className="address" required />
         <br />
         <label>areaCode</label>
-        <input value={""} type="text" className="areaCode" required />
+        <input id="areaCode" value={""} onChange={formHandle} type="text" className="areaCode" required />
         <br />
         <label>rent</label>
-        <input value={""} type="text" className="rent" required />
+        <input id="rent" value={""} onChange={formHandle} type="text" className="rent" required />
         <br />
         <label>preferredTenant</label>
         <br />
         <label>bachelor</label>
-        <input checked={""} type="checkbox" className="bachelor" />
+        <input id="preferredTenant" onChange={formHandle} checked={""} value={"bachelor"} type="checkbox" className="bachelor" />
         <br />
         <label>married</label>
-        <input checked={""} type="checkbox" className="married" />
+        <input id="preferredTenant" onChange={formHandle} checked={""} value={"married"} type="checkbox" className="married" />
         <br />
         <label>image</label>
-        <input value={""} type="text" className="image" required />
+        <input id="image" value={""} onChange={formHandle} type="text" className="image" required />
         <br />
         <input className="submitBtn" type="submit" />
       </form>
